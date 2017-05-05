dbf22e63acc203fe8165fb28bb28f65459467190
ngeorgieva <nat.m.georgieva@gmail.com>
2017-05-05 18:27:05 +0300

    New tests added

---
+++ Sightseer.Test/Services/TestHomeService.cs
@@ -0,0 +1,29 @@
+﻿namespace Sightseer.Test.Services
+{
+    using System.Linq;
+    using AutoMapper;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Models.EntityModels;
+    using Models.ViewModels.Attractions;
+    using Sightseer.Services;
+
+    [TestClass]
+    public class TestHomeService
+    {
+        [TestMethod]
+        public void GetTopAttractions()
+        {
+            var service = new HomeService();
+
+            Mapper.Initialize(cfg =>
+            {
+                cfg.CreateMap<Attraction, AttractionVm>();
+            });
+
+            var result = service.GetTopAttractions();
+
+            Assert.AreEqual(3, result.Count());
+            Assert.AreEqual(true, result.All(a => a.Rating >= 4));
+        }
+    }
+}
---
+++ Sightseer.Test/Services/TestReviewsService.cs
@@ -0,0 +1,45 @@
+﻿namespace Sightseer.Test.Services
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using AutoMapper;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Models.EntityModels;
+    using Models.ViewModels.Reviews;
+    using Sightseer.Services;
+    using Sightseer.Services.Interfaces;
+
+    [TestClass]
+    public class TestReviewsService
+    {
+        private IReviewsService service;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            this.service = new ReviewsService();
+        }
+
+        //[TestMethod]
+        //public void GetAllReviewsForAttraction_ValidCase_ShouldReturnReviews()
+        //{
+        //    Mapper.Initialize(cfg =>
+        //    {
+        //        cfg.CreateMap<IEnumerable<Review>, IEnumerable<ReviewVm>>();
+        //    });
+
+        //    var result = this.service.GetAllReviewsForAttraction(1);
+
+        //    Assert.AreEqual(4, result.Count());
+        //    Assert.AreEqual(typeof(IEnumerable<ReviewVm>), result.GetType());
+        //}
+
+        [TestMethod]
+        public void GetAllReviewsForAttraction_InvalidCase_ShouldReturnNull()
+        {
+            var result = this.service.GetAllReviewsForAttraction(100);
+
+            Assert.AreEqual(null, result);
+        }
+    }
+}
